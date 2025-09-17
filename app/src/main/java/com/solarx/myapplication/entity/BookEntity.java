bfeb711586e5521a06eaaf5ad4164512b9c6d13d
Thiago Candido <thiagocarvalhocandido8@gmail.com>
2025-09-16 21:50:56 -0300

    mapping book entity

--- app/src/main/java/com/solarx/myapplication/entity/BookEntity.java
+++ app/src/main/java/com/solarx/myapplication/entity/BookEntity.java
@@ -3,8 +3,36 @@ package com.solarx.myapplication.entity;
 public class BookEntity {
 
     private final int id;
+    private final String title;
+    private final String author;
+    private final boolean favorite;
+    private final String genre;
 
-    public BookEntity(int id){
+    public BookEntity(int id, String title, String author, boolean favorite, String genre) {
         this.id = id;
+        this.title = title;
+        this.author = author;
+        this.favorite = favorite;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public String getGenre() {
+        return genre;
     }
 }
